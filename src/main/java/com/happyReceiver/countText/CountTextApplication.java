436750ea5ad4838777b91ba0d68453e0b6b7a6b0
김희수 <heesukim@kakao.com>
2020-12-27 21:57:13 +0900

    #init

---
+++ src/main/java/com/happyReceiver/countText/CountTextApplication.java
@@ -0,0 +1,13 @@
+package com.happyReceiver.countText;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountTextApplication.class, args);
+	}
+
+}
