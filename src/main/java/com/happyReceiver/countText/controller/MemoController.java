b213bc1a574a2c4a73b58be8c10c21adcaa83e9b
김희수 <heesukim@kakao.com>
2021-01-01 01:45:58 +0900

    re

--- src/main/java/com/happyReceiver/countText/controller/MemoController.java
+++ src/main/java/com/happyReceiver/countText/controller/MemoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/memo")
+@RequestMapping("/api/memo")
 public class MemoController {
 
     @Autowired
